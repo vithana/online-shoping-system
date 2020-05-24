1c478e6339fdf5db59eeb5564234d5726eadf60a
Pasindu <pasinduhavpu1998@gmail.com>
2020-05-24 23:05:36 +0530

    Single Product Header Edit

--- client/src/components/Header/StoreManagerHeader.js
+++ client/src/components/Header/StoreManagerHeader.js
@@ -96,7 +96,7 @@ class StoreManagerHeader extends React.Component {
             <div className="header-body">
               {/* Card stats */}
               <Row>
-                <Col lg="6" xl="3">
+                <Col lg="6" xl="4">
                   <Card className="card-stats mb-4 mb-xl-0">
                     <CardBody>
                       <Row className="my-2">
@@ -118,7 +118,7 @@ class StoreManagerHeader extends React.Component {
                     </CardBody>
                   </Card>
                 </Col>
-                <Col lg="6" xl="3">
+                <Col lg="6" xl="4">
                   <Card className="card-stats mb-4 mb-xl-0">
                     <CardBody>
                       <Row className="my-2">
@@ -142,7 +142,7 @@ class StoreManagerHeader extends React.Component {
                     </CardBody>
                   </Card>
                 </Col>
-                <Col lg="6" xl="3">
+                <Col lg="6" xl="4">
                   <Card className="card-stats mb-4 mb-xl-0">
                     <CardBody>
                       <Row className="my-2">
